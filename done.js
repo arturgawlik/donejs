4dbf21578b554b09bbc2551984b008ea35b2881e
arturgawlik <artur.gawlik@icloud.com>
2025-03-20 21:01:45 +0100

    add initial `fetch` global function

--- done.js
+++ done.js
@@ -1,8 +1,10 @@
 import doneJsConsole from "/home/artur-gawlik/git/donejs/lib/console.js";
 import doneJsProcess from "/home/artur-gawlik/git/donejs/lib/process.js";
 import doneJsTextDecoder from "/home/artur-gawlik/git/donejs/lib/text-decoder.js";
+import doneJsFetch from "/home/artur-gawlik/git/donejs/lib/fetch.js";
 
 globalThis.console = doneJsConsole;
 globalThis.process = doneJsProcess;
 globalThis.exit = doneJsProcess.exit;
 globalThis.TextDecoder = doneJsTextDecoder
+globalThis.fetch = doneJsFetch;
--- samples/test.js
+++ samples/test.js
@@ -1,20 +1,7 @@
 try {
-    const resGetAddrInfo = {};
-    globalThis.done.syscall.getaddrinfo(null, "5001", { ai_family: 'AF_UNSPEC', ai_socktype: 'SOCK_STREAM' }, resGetAddrInfo);
-    const socketFd = globalThis.done.syscall.socket(resGetAddrInfo.ai_family, resGetAddrInfo.ai_socktype, resGetAddrInfo.ai_protocol);
-    const connectRes = globalThis.done.syscall.connect(socketFd, resGetAddrInfo.ai_addr, resGetAddrInfo.ai_addrlen);
+    const res = fetch("http://localhost:5001");
+    console.log(`recv data decoded: ${res}`);
 
-    const bufferSize = 1024;
-    const buffer = new ArrayBuffer(bufferSize);
-    const recvConnect = globalThis.done.syscall.recv(socketFd, buffer, bufferSize, 0);
-
-    const decoder = new TextDecoder();
-    const decodedText = decoder.decode(buffer);
-
-    console.log(`recv byte count: ${recvConnect}`);
-    console.log(`recv data decoded: ${decodedText}`);
-
-    const closeRes = globalThis.done.syscall.close(socketFd);
 } catch (e) {
     console.log(e.message);
 }
