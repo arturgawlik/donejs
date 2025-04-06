77cf7cd750998a1ee6d8b0aad3661d0a37a7569d
arturgawlik <artur.gawlik@icloud.com>
2025-04-06 14:42:43 +0200

    add flags naive interpreter in js land

--- done.js
+++ done.js
@@ -2,6 +2,7 @@ import doneConsole from "done:console.js";
 import doneProcess from "done:process.js";
 import doneTextDecoder from "done:text-decoder.js";
 import doneFetch from "done:fetch.js";
+import doneInterpretFlags from "done:argv.js";
 import "done:command.js";
 
 globalThis.console = doneConsole;
@@ -9,3 +10,6 @@ globalThis.process = doneProcess;
 globalThis.exit = doneProcess.exit;
 globalThis.TextDecoder = doneTextDecoder
 globalThis.fetch = doneFetch;
+
+globalThis.done = {};
+globalThis.done.interpretFlags = doneInterpretFlags;
